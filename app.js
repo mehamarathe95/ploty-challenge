aad30e1d88a47428cf82a61d42bac0623d400c29
Meha Marathe <mehamarathe@mehas-air.home>
2020-11-06 01:31:35 -0800

    second commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//1. Use the D3 library to read in `samples.json`.
+//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+//* Use `sample_values` as the values for the bar chart.
+//* Use `otu_ids` as the labels for the bar chart.
+//* Use `otu_labels` as the hovertext for the chart.
+//![bar Chart](Images/hw01.png)
+
+//1. Use the D3 library to read in `samples.json`.
+d3.json("../../samples.json", function(data) {
+        console.log(data);
+    });
